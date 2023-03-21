85aaf52319a5f7ffaf65d7d084fd705db5e84371
Pepper2576 <107074452+Pepper2576@users.noreply.github.com>
2023-03-21 16:40:14 +0000

    testing beta

--- cypress/e2e/shop.cy.js
+++ cypress/e2e/shop.cy.js
@@ -6,8 +6,18 @@ describe("Shop page test", () => {
 		cy.url().should("include", "/shop");
 		cy.get("[data-cy=1]").click();
 		cy.get("[data-cy=modalBtn]").click();
+		cy.get('[data-cy="cartSubtotal"]').should("contain", `£50`);
+		cy.get('[data-cy="cartSubtotalTax"]').should("contain", `£55`);
+		cy.get('[data-cy="totalPlusShipping"]').should("contain", `£75`);
+
 		cy.get("[data-cy=removeAllBtn]").contains("Remove All").click();
 		cy.get("[data-cy=checkoutBtn]").contains("Checkout");
 		cy.get("[data-cy=modalCloseBtn]").contains("X").click();
+		cy.request({
+			method: "GET",
+			url: "http://localhost:3000/",
+		}).then((response) => {
+			expect(response.status).to.eq(200);
+		});
 	});
 });
