2d9fab2afcd7682c2ac5b1465b8e65bd2d8d0d85
Pepper2576 <107074452+Pepper2576@users.noreply.github.com>
2023-04-13 09:03:22 +0100

    MVP ver 5

--- src/Pages/Shop/Shop.cy.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Shop from './Shop'
-
-describe('<Shop />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Shop />)
-  })
-})
